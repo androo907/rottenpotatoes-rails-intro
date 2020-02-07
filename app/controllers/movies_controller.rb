76892b3eebeca7207b4026d1d688b3cb35f2c0ff
EC2 Default User <ec2-user@ip-172-31-93-242.ec2.internal>
2020-02-07 00:36:45 +0000

    finished everything

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,10 +11,30 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.order(params[:order])
-    if params[:order] == 'title'
+    @all_ratings = ['G','PG','PG-13','R']
+    @saved_ratings = {'G'=>'1','PG'=>'1','PG-13'=>'1','R'=>'1'}
+    if params[:commit] == 'Refresh'
+      session[:ratings] = params[:ratings]
+    end
+    
+    if params[:order] == 'title' || params[:order] == 'release_date'
+      session[:order] = params[:order]
+    end
+    
+    if session[:ratings].nil?
+      session[:ratings] = @saved_ratings
+    end
+    
+    @saved_ratings.each do |rating|
+      @saved_ratings = session[:ratings]
+    end
+      
+    @movies = Movie.order(session[:order]).with_ratings(session[:ratings].keys)
+    
+    
+    if session[:order] == 'title'
       @title_header = 'hilite'
-    elsif params[:order] == 'release_date'
+    elsif session[:order] == 'release_date'
       @release_date_header = 'hilite'
     end
   end
@@ -45,10 +65,6 @@ class MoviesController < ApplicationController
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
-  end
-  
-  def sortTitle
-    
   end
 
 end
